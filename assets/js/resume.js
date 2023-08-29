f7ee3ceea506563e02ea7ed157e921ea6b6cb753
Marijn Grootjans <marijngrootjans@gmail.com>
2023-08-29 09:46:37 +0200

    Moved openlayers stamen tiles map to top

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -48,21 +48,13 @@
         var logo = new ol.Feature({
             geometry: new ol.geom.Point(ol.proj.fromLonLat([3.440047, 51.527508]))
         });
-        var svg = '<svg viewBox="21.948 21.948 263.445 285.324" width="46.1667" height="50" xmlns="http://www.w3.org/2000/svg">' 
-        + '<g transform="matrix(1, 0, 0, 1, -65.844002, -109.740013)">'
-        + '<title>mg</title>'
-        + '<path style="stroke: rgb(0, 0, 0); fill: rgb(68, 68, 68); stroke-width: 0px;" d="M 351.237 197.33 L 307.272 219.48 L 307.272 197.532 L 219.48 241.428 L 131.688 197.532 L 131.688 307.272 L 87.792 285.324 L 87.792 131.688 L 219.48 197.532 L 351.168 131.688 L 351.168 197.532">'
-        + '<title>m</title>'
-        + '</path>'
-        + '<path style="stroke: rgb(0, 0, 0); fill: rgb(68, 68, 68); stroke-width: 0px;" d="M 131.455 329.067 L 219.48 373.116 L 307.272 329.22 L 307.272 285.324 L 219.48 329.22 L 219.48 285.324 L 307.272 241.428 L 351.168 219.48 L 351.168 351.168 L 219.48 417.012 L 87.792 351.168 L 87.792 307.272 L 131.688 329.22">'
-        + '<title>g</title>'
-        + '</path>'
-        + '</g>'
+        var svg = '<svg height="100" width="100" xmlns="http://www.w3.org/2000/svg">'
+        + '<circle cx="50" cy="50" r="30" stroke="white" stroke-width="15" fill="rgb(68, 68, 68)" />'
         + '</svg>';
         logo.setStyle(new ol.style.Style({
             image: new ol.style.Icon({
                 src: 'data:image/svg+xml;base64,' + window.btoa(svg),
-                scale: 1
+                scale: 0.25
             })
         }));
         var map = new ol.Map({
